0c60aad3e2c61bfd0840d5ffff315b4f593b54d0
Anton Vasetenkov <antvaset@gmail.com>
2025-09-20 10:38:31 +1200

    Upgrade to the new CQL-to-ELM 4.0.0-SNAPSHOT (#47)
    * Upgrade to the new CQL-to-ELM 4.0.0-SNAPSHOT
    * Use Java 17 in CI and Docker

--- src/test/java/org/mitre/bonnie/cqlTranslationServer/TranslationResourceTest.java
+++ src/test/java/org/mitre/bonnie/cqlTranslationServer/TranslationResourceTest.java
@@ -41,7 +41,6 @@ import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.w3c.dom.Document;
 import org.xml.sax.InputSource;
@@ -195,7 +194,7 @@ public class TranslationResourceTest {
     assertEquals("include", errorAnnotation.getString("errorType"));
     assertEquals(5, errorAnnotation.getInt("startLine"));
     assertEquals(1, errorAnnotation.getInt("startChar"));
-    assertTrue(errorAnnotation.getString("message").matches("Could not load source for library CMSAll,\\s+version 1."));
+    assertEquals("Could not load source for library CMSAll, version 1, namespace uri null.", errorAnnotation.getString("message"));
   }
 
   @Test
@@ -313,7 +312,6 @@ public class TranslationResourceTest {
     }
   }
 
-  @Disabled("XML translation throws java.lang.IllegalStateException: 'Cache size exceeded expected bounds!'")
   @Test
   void testMultipartRequestAsXml() throws Exception {
     String filenames[] = {"valid.cql"};
@@ -338,7 +336,6 @@ public class TranslationResourceTest {
     }
   }
 
-  @Disabled("XML translation throws java.lang.IllegalStateException: 'Cache size exceeded expected bounds!'")
   @Test
   void testMultipartRequestAsJsonAndXml() throws Exception {
     String filenames[] = {"valid.cql"};
